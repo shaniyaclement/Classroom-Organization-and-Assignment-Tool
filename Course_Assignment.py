f44880419af9b2aedd73ca130b70d93f4747fd83
Shaniya Clement <shaniyaclement17@gmail.com>
2023-11-26 18:41:18 -0600

    Uploading/ updating the CSV files and Course_Assignement instruction. The instructions currently account for online classes, assuring no availability overlap, and within classroom capacity. It ensures that the csv files are read and new files are written for newly assigned and conflicting courses. Last required implementation will be assuring courses are only assigned within their correct building.

--- Course_Assignment.py
+++ Course_Assignment.py
@@ -1,48 +1,128 @@
+import os
 import csv
+from datetime import datetime
 
 class Assignment:
-    file_path = 'Classrooms_Demo.csv'
-    with open(file_path, 'r', '') as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip the header row
-   
+    classrooms = 'Classrooms_Demo.csv'
+    course_catalog = 'Course_Catalog.csv'
+    assigned_courses = 'Assigned_Courses.csv'
+    conflicting_courses = 'Conflicting_Courses.csv'
+    assigned_buildngs = {'ART': ['JUB'], 'ACC': ['PARKJ']}
+
     # Initialize a dictionary to track classroom availability
-    classroom_availability = {}
-    for row in reader:
-        classroom_name = row['Name']  # Name is in column 5
-        classroom_availability[classroom_name] = {day: [] for day in ['M', 'T', 'W', 'TR', 'F']}
-    #{"NEWLV 001": {'M': []}}
-    
-    # TODO: Assigns Course to a Classroom
-    def assign_classroom(classroom_availability):
-        file_path = 'Course_Catalog.csv'
-        with open(file_path, 'r', '') as file:
+    def assign_availability(classrooms):
+        classroom_availability = {}
+        classrooms = classrooms
+        with open(classrooms, 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
-        # for each line course read, implement logic to assign it to a classroom -- checks all classroom until it finds one that is available
-        # access the course days, split it, loop through calling check_classroom_availibility to check it it is available for all needed days
-        # call function that blocks the time out and call function if available for all days
-        pass
+            for row in reader:
+                classroom_name = row[4]  # Name is in column 5
+                classroom_availability[classroom_name] = {day: [] for day in ['M', 'T', 'W', 'TR', 'F']}
+                classroom_availability[classroom_name]['info'] = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]]
+        return classroom_availability
+    
+    # Turns string of course days into list where TR is represented as one day
+    def split_days_string(course_days):
+        course_days_split = []
+        i = 0
+        while i < len(course_days):
+            current_char = course_days[i]
+            course_days_split.append(current_char)
+            if current_char == 'T' and i + 1 < len(course_days) and course_days[i + 1] == 'R':
+                course_days_split[-1] = 'TR'
+                i += 1 
+            i += 1
+        return course_days_split
+    
+    def check_classroom_availability(classroom, course_start, course_end, course_days, course_capacity):
+        # Check if course capacity exceeds the room capacity
+        if (int(course_capacity) > int(classroom['info'][9])):
+            print(f"Exceeds capacity: {course_capacity} > {classroom['info'][9]}")
+            return False
+        new_interval = (course_start, course_end)
+        for day in course_days:
+            for course_time in classroom[day]:
+                existing_interval = (course_time[0], course_time[1])
+                if not (new_interval[1] <= existing_interval[0] or new_interval[0] >= existing_interval[1]):
+                    print(f"Overlap found: {classroom['info'][4]} is not available on {day} from {course_start} to {course_end}.")
+                    return False  # Overlap found
+        return True  # No overlap, classroom available on each needed day    
 
-    def check_classroom_availability(classroom_availability, start, end, day):
-        # Iterate through the classrooms: key = room, value = availability
-        for room, availability in classroom_availability.items():
-            # Check if the day is in the availability dictionary.
-            if day in availability:
-                # Check if the room is available during the requested time range
-                if is_room_available(availability[day], start, end):
-                    # If the room is available, print a message indicating its availability and return true.
-                    print(f"{room} is available on {day} from {start} to {end}")
-                    return True
+    def add_course(classroom, course, course_start, course_end, course_days):
+        if 'ONL' not in course[1]:
+            new_interval = (course_start, course_end)
+            for day in course_days:
+                classroom[day].append(new_interval)
+        course = ','.join(course)
+        classroom = ','.join(classroom['info'])
 
-    # Check if a room is available during a specified time range
-    def is_room_available(availability, start, end):
-        # Iterate through the list of time frames for the room 
-        for time in availability:  # time_frame is a tuple in availability, which is a list of tuples
-            if start < time[1] and end > time[0]:
-                # Check if there is overlap between the requested time and booked time
-                return False
-        # If no overlap is found, return True
-        return True
+        # Check if the CSV file exists
+        if not os.path.exists(Assignment.assigned_courses):
+            # Read header from Course_Catalog.csv
+            with open(Assignment.course_catalog, 'r') as file:
+                course_header = file.readline().strip()  # strip() removes leading/trailing whitespaces
+            # Read headers from Classrooms_Demo.csv
+            with open(Assignment.classrooms, 'r') as file:
+                classroom_header = file.readline().strip()
+            # Combine headers
+            header = course_header + ',' + classroom_header
 
-    # TODO: implement a function that adds the tuple blocking that time off to the program
+            with open(Assignment.assigned_courses, 'w') as csv_file:
+                csv_file.write(header + '\n')  # Write the header
+
+        # Add a course to the end of the CSV file
+        with open(Assignment.assigned_courses, 'a') as csv_file:
+            csv_file.write(course + ',' + classroom + '\n')
+
+    def conflicts(course):
+        course = ','.join(course)
+
+        # Check if the CSV file exists
+        if not os.path.exists(Assignment.conflicting_courses):
+            # Read header from Course_Catalog.csv
+            with open(Assignment.course_catalog, 'r') as file:
+                header = file.readline().strip()
+            with open(Assignment.conflicting_courses, 'w') as csv_file:
+                csv_file.write(header + '\n')  # Write the header
+
+        # Add course to the end of the CSV file
+        with open(Assignment.conflicting_courses, 'a') as csv_file:
+            csv_file.write(course + '\n')
+       
+    def assign_classroom():
+        classroom_availability = Assignment.assign_availability(Assignment.classrooms)
+        # Read the course catalog
+        file_path = Assignment.course_catalog
+        with open(file_path, 'r') as file:
+            course_reader = csv.reader(file)
+            next(course_reader) # Skip the header row
+            for course in course_reader:
+                # Split the course days so it's easier to check for each day
+                department = course[0]
+                course_id = course[1]
+                course_name = course[2]
+                course_days = Assignment.split_days_string(course[3])
+                # Convert strings to time objects
+                course_start = datetime.strptime(course[4], '%H%M').time()
+                course_end = datetime.strptime(course[5], '%H%M').time()
+                course_capacity = course[6]
+                course = [department, course_id, course_name, course[3], course[4], course[5], course_capacity]
+                
+                # Online courses do not need room assignments 
+                if 'ONL' in course_id:
+                    print("ONL course")
+                    Assignment.add_course({'info':['Main','Online','ONL',',','Online','Online','Online','Online',',']}, course, course_start, course_end, course_days)
+                else:
+                    print(course_name)
+                    # while loop somewhere
+                    for classroom in classroom_availability:
+                        available = Assignment.check_classroom_availability(classroom_availability[classroom], course_start, course_end, course_days, course_capacity)
+                        if available == True:
+                            Assignment.add_course(classroom_availability[classroom], course, course_start, course_end, course_days)
+                            break
+                    if available == False:
+                        Assignment.conflicts(course)
+                       
+# TODO: rooms need to be in a specific building
+# Accounts for ONL classes, write courses with conflicts to a seperate file, classroom capacity,
